763774bef045b08e6637104608567215ee6a68fd
Joep de Ligt <joepio@gmail.com>
2015-07-09 15:03:34 +0200

    Adding alternative options

--- Count_Telomeric_Sequence_Reads.py
+++ Count_Telomeric_Sequence_Reads.py
@@ -1,28 +1,42 @@
 #!/usr/bin/python2.7
 
 import pysam
+import glob
 import os
 
 from optparse import OptionParser
-
+# -------------------------------------------------
 parser = OptionParser()
-parser.add_option("--sambamba",	 dest="sambamba", help="Path to sambamba/samtools executable",   default="sambamba")
-parser.add_option("--bamdir",	 dest="bamdir",   help="Path to directory containing bam files", default=False)
-parser.add_option("--outdir",	 dest="outdir",   help="Path to directory to write output to",   default="./telomeres/")
+parser.add_option("--sambamba",	 dest="sambamba", help="Path to sambamba/samtools executable", default="sambamba")
+parser.add_option("--bamdir",	 dest="bamdir",   help="Path to directory containing BAM files", default=False)
+parser.add_option("--outdir",	 dest="outdir",   help="Path to directory to write output to", default="./telomeres/")
+parser.add_option("--repsize",   dest="repsize",  help="Number of required matching 6mers (TTAGGG)", default=10)
 
 (options, args) = parser.parse_args()
+# -------------------------------------------------
 
-#TODO implement argparse to get these variables
-sambamba = "/path/to/sambamba/sambamba_vx.x"
-workdir = "/path/to/working/directory/"
-searchdir = "/path/to/search/bams/"
+def check_arguments():
+    if not os.path.exists(options.bamdir):
+        print("Invalid BAM folder %s"%(options.bamdir))
+        return False
+
+    if not os.path.exists(options.outdir):
+        print("Creating output folder %s"%(options.outdir))
+        try:
+            os.makedir(options.outdir)
+        except OSError:
+            print("Invalid / unable to create, output folder %s"%(options.outdir))
+            return False
+        
+    return True
 
 # -------------------------------------------------
 
 def count_telomeric_reads(bamfile, telofile):
+    # check if the file was already generated
     if not os.path.exists(telofile):
         # extract telomeric reads and write to file
-        os.system(sambamba + " view " + bamfile + " | LC_ALL=C grep -E \"" + "TTAGGG"*15 +"|"+ "CCCTAA"*15 + "\"" + " > " + telofile + " & ")
+        os.system(options.sambamba + " view " + bamfile + " | LC_ALL=C grep -E \"" + "TTAGGG"*options.repsize +"|"+ "CCCTAA"*options.repsize + "\"" + " > " + telofile + " & ")
 
     # count total number of reads
     total_rc = reduce(lambda x, y: x + y, [ eval('+'.join(l.rstrip('\n').split('\t')[2:]) ) for l in pysam.idxstats(bamfile) ])
@@ -33,29 +47,35 @@ def count_telomeric_reads(bamfile, telofile):
     # print counts
     print(bamfile, total_rc, telomere_rc)
     
+# -------------------------------------------------
+
+def main():
+    # check specified options
+    if not check_arguments():
+        return 1
     
-def main():    
-    for root, dirs, files in os.walk(searchdir):
-        for filename in files:
-            # select bam files
-            if filename.endswith(".bam"):
-                #print(filename)
-                bamfile = os.path.join(root, filename)
-                baifile = bamfile+".bai"
-                if not os.path.exists(baifile):
-                    print("No index file found, indexing")
-                    os.system(sambamba + " index " + bamfile)
-                    print("Indexing performed")
-                    
-                telofile = workdir+filename.replace(".bam","_TelomericReads.sam")
-            
-                count_telomeric_reads(bamfile, telofile)
+    # for all bamfiles
+    for filename in os.path.join(options.bamdir, "*.bam"):
+        #print(filename)
+        bamfile = os.path.join(options.bamdir, filename)
+        baifile = bamfile+".bai"
+        
+        # check if index file exists
+        if not os.path.exists(baifile):
+            print("No index file found, indexing now")
+            os.system(sambamba + " index " + bamfile)
+            print("Indexing performed")
+        
+        # generate Telomere reads file name   
+        telofile = os.path.join(options.outdir, filename.replace(".bam","_TelomericReads.sam"))
+        
+        # generate Telomere reads file
+        count_telomeric_reads(bamfile, telofile)
 
 # -------------------------------------------------
 # Execute program
-
+# -------------------------------------------------
 print("Starting analysis")
 main()
 print("DONE")
-
-
+# -------------------------------------------------
