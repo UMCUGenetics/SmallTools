f4e4a788089c7a8bbd498649a1a8cd6ad14da2a2
Joep de Ligt <joepio@gmail.com>
2015-07-09 15:24:14 +0200

    Update Count_Telomeric_Sequence_Reads.py

--- Count_Telomeric_Sequence_Reads.py
+++ Count_Telomeric_Sequence_Reads.py
@@ -61,7 +61,7 @@ def main():
     # open output file
     output = open(os.path.join(options.outdir, "TelomereCounts.txt"),'w')
     # write header
-    output.write('\t'.join("#Sample","TotalReads","TelomericReads","NormalisedFraction")+'\n')
+    output.write('\t'.join(["#Sample","TotalReads","TelomericReads","NormalisedFraction"])+'\n')
         
     # for all bamfiles
     for filename in glob.glob(os.path.join(options.bamdir, "*.bam")):
@@ -81,7 +81,7 @@ def main():
         # generate Telomere reads file
         print(bamfile,telofile)
         counts = count_telomeric_reads(bamfile, telofile)
-        output.write('\t'.join(filename,counts[0], ((counts[1],counts[1]/counts[0])*1000))+'\n')
+        output.write('\t'.join([filename,counts[0], ((counts[1],counts[1]/counts[0])*1000)])+'\n')
     
     output.close()
 
