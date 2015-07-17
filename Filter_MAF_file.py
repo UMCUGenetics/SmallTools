cb2d36b3baeb3a2e796c58e2d3ee476bcbc4e47d
Joep de Ligt <joepio@gmail.com>
2015-07-17 16:19:19 +0200

    Update and rename FilterMAFfile.py to Filter_MAF_file.py

--- Filter_MAF_file.py
+++ Filter_MAF_file.py
@@ -4,17 +4,12 @@
 import os
 from itertools import dropwhile, islice
 from math import log
-
-# MAF handling
-from pysam import AlignmentFile, AlignedSegment
-
-
 from optparse import OptionParser
 
 # ------------------------------------------------------------------------------------------------------------------------
 
-class LASTregion:
-	"""LAST region class"""
+class MAFregion:
+	"""MAF region class"""
 
 	def __init__(self, array):
 		self.loc = array[1]
@@ -37,14 +32,16 @@ class LASTregion:
 			return 24
 		else:
 			return int(self.loc)
+	def to_maf(self):
+		return "s %s\t\t%s %s %s %s %s"%(self.loc, self.pos, self.length, self.strand,"00000",self.seq)
 
-class LASTmapping:
-	"""LAST mapping class"""
+class MAFmapping:
+	"""MAF mapping class"""
 	
 	def __init__(self, score, ref, aln):
 		self.score = score
-		self.ref = LASTregion(ref)
-		self.aln = LASTregion(aln)
+		self.ref = MAFregion(ref)
+		self.aln = MAFregion(aln)
 
 	def __eq__(self, other):
 		return self.score == other.score
@@ -60,13 +57,17 @@ class LASTmapping:
 	
 	def __str__(self):
 		return "%i %s -> %s" %(self.score, self.aln, self.ref)
+		
+	def to_maf(self):
+		return "a score=%s\n%s\n%s\n"%(self.score, self.ref.to_maf(), self.aln.to_maf())
+		
 
 
 # ------------------------------------------------------------------------------------------------------------------------
 
 parser = OptionParser()
-parser.add_option("--bam",   dest="bam_file",	   help="Path of BAM file to parse",		default=False)
-parser.add_option("--maf",   dest="maf_file",	   help="Path of MAF file to parse",		default=False)
+parser.add_option("--maf",   dest="maf_file",	 help="Path of input MAF file to parse",	default=False)
+parser.add_option("--out",   dest="out_file",	 help="Path of output MAF file to write",	default=False)
 parser.add_option("--qual",  dest="qual_score",  help="Minimum quality for alignments",		default=300)
 (options, args) = parser.parse_args()
 
@@ -74,22 +75,17 @@ parser.add_option("--qual",  dest="qual_score",  help="Minimum quality for align
 
 def check_arguments(options):
 	#print("Checking arguments")
-	if not os.path.exists(options.bam_file):
-		print("Invalid BAM file %s"%(options.bam_file))
-		return False
-
 	if not os.path.exists(options.maf_file):
 		print("Invalid MAF file %s"%(options.maf_file))
 		return False
 
 	return True
 
-
-
 def isHeaderLine(line):
 	return line.startswith("#")
 
-def gather_alt_mappings(options, collection):
+def filter_alt_mappings(options):
+	collection = {}
 	# Parse MAF file
 	with open(options.maf_file,'r') as f:
 		for line in dropwhile(isHeaderLine, f):
@@ -110,18 +106,34 @@ def gather_alt_mappings(options, collection):
 				strand = -1
 
 			if (score >= options.qual_score):
-			  if (aln[1] not in collection):
-          collection[aln[1]] = []
-				collection[aln[1]].append(LASTmapping(score, ref, aln))
+				if (aln[1] not in collection):
+		          		collection[aln[1]] = []
+				collection[aln[1]].append(MAFmapping(score, ref, aln))
 
-				#print "%i %s:%s-%s  -> %s:%s-%s" %(score, aln[1], aln[2], aln[3], ref[1], ref[2], ref[3])
+			#print "%i %s:%s-%s  -> %s:%s-%s" %(score, aln[1], aln[2], aln[3], ref[1], ref[2], ref[3])
+	return collection
 
+def write_alt_mappings(options, collection):
+	outf = open(options.out_file, 'w')
+	
+	inf = open(options.maf_file,'r')
+	for line in inf:
+		if isHeader(line):
+			outf.write(line)
+		else
+			break
+	inf.close()
+	
+	for read in collection:
+		for aln in colleaction[read]:
+			outf.write(aln.to_maf())
+	outf.close()
+	
 # ------------------------------------------------------------------------------------------------------------------------
 
 if check_arguments(options):
-	collection = {}
-	gather_alt_mappings(options, collection)
-	write_alt_mappings(options, collection)
+	filtered_reads = filter_alt_mappings(options)
+	write_alt_mappings(options, filtered_reads)
 
 print("DONE")
 
