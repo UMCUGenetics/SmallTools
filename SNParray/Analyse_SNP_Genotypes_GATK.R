52f17a435ca766003d7b8b674045fa80ef2bc4ef
Joep de Ligt <joepio@gmail.com>
2016-05-11 09:46:49 +0200

    Update Analyse_SNP_Genotypes_GATK.R

--- SNParray/Analyse_SNP_Genotypes_GATK.R
+++ SNParray/Analyse_SNP_Genotypes_GATK.R
@@ -77,6 +77,7 @@ make_heatmap <- function(data, clusteringflag, groupcols) {
   heatmappy <- heatmap.2(data, trace="none", scale="none", margins=c(10, 10), col=matrixcols2, cexCol=1.2, cexRow=1.4, dendrogram=clusteringflag, Rowv=rowflag, Colv=colflag, symkey=F,                           sepwidth=c(0.02,0.02), sepcolor="lightgray", colsep=1:ncol(data), rowsep=1:nrow(data), keysize=0.5)
   if (! is.na(groupcols)) {
   heatmappy <- heatmap.2(data, trace="none", scale="none", margins=c(10, 10), col=matrixcols2, cexCol=1.2, cexRow=1.4, dendrogram=clusteringflag, Rowv=rowflag, Colv=colflag, symkey=F,  RowSideColors=groupcols, sepwidth=c(0.02,0.02), sepcolor="lightgray", colsep=1:ncol(data), rowsep=1:nrow(data), keysize=0.5)
+  }
   return(heatmappy)
 }
 
