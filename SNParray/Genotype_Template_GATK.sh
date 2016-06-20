8b91090f546be8546797efc33951e05d1326141c
jdeligt <joepio@gmail.com>
2016-06-20 15:53:52 +0200

    Adding Genotyping script

---
+++ SNParray/Genotype_Template_GATK.sh
@@ -0,0 +1,19 @@
+# SETTINGS
+ref="/../../Homo_sapiens.GRCh37.GATK.illumina/Homo_sapiens.GRCh37.GATK.illumina.fasta"
+snp="taqman_design.vcf"
+gatk="/../../GenomeAnalysisTK-3.2-0/GenomeAnalysisTK.jar"
+call="UnifiedGenotyper"
+panel="WGS_32SNP_OpenArrayCalls"
+
+# First argument is bamfolder
+folder=$1
+outfolder=$2
+
+for bam in $folder/*.bam; do
+	filename=${bam##*/}
+	samplename=${filename%.*}
+	outfile="${outfolder}${samplename}_${panel}.vcf"
+
+	echo $samplename
+	java -Xms2g -Xmx9g -jar ${gatk} -T ${call} -R ${ref} -L ${snp} --output_mode EMIT_ALL_SITES -I ${bam} -o ${outfile}
+done
