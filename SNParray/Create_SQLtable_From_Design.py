a58388b42e92b571d6f55625204d343246f70813
jdeligt <joepio@gmail.com>
2016-09-23 13:05:20 +0200

    code cleanup

--- SNParray/Create_SQLtable_From_Design.py
+++ SNParray/Create_SQLtable_From_Design.py
@@ -13,19 +13,19 @@ if not os.path.exists(options.vcf_file):
 	exit(0)
 
 
-TABLE_NAME = os.path.basename(options.vcf_file)
+TABLE_NAME = os.path.basename(options.vcf_file).replace("_design.vcf","")
 VCF_READER = vcf.Reader(open(options.vcf_file, 'r'))
 
-SNP_TEMPLATE = """'%s' tinyint(3) NOT NULL"""
-SQL_TEMPLATE = """CREATE TABLE '%s' (
-'Sample' varchar(100) NOT NULL,
+SNP_TEMPLATE = """%s tinyint(3) NOT NULL"""
+SQL_TEMPLATE = """CREATE TABLE %s (
+Sample varchar(100) NOT NULL,
 %s,
-PRIMARY KEY ('Sample')
-)
+PRIMARY KEY (Sample)
+);
 """
 
 SNPids = []
 for vcf_record in VCF_READER:
 	SNPids.append(vcf_record.ID)
 
-print SQL_TEMPLATE%(TABLE_NAME.replace("_design.vcf",""),",\n".join([SNP_TEMPLATE%(x) for x in SNPids]))
+print SQL_TEMPLATE%(TABLE_NAME,",\n".join([SNP_TEMPLATE%(x) for x in SNPids]))
