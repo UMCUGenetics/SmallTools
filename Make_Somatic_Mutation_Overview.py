6e73cf8803c50264bdef52519fdece47be31a4dd
jdeligt <joepio@gmail.com>
2017-02-28 14:52:31 +0100

    Adding more debug

--- Make_Somatic_Mutation_Overview.py
+++ Make_Somatic_Mutation_Overview.py
@@ -264,6 +264,7 @@ def main():
                     proteffect=pred.split("|")[10]
                     break
             if (debug):
+                print(thisrec.INFO["ANN"])
                 print(gene, sample, proteffect, mapping[rdf[sample][gene]["EFF"]], str(thisrec.CHROM), str(thisrec.POS), str(thisrec.POS+len(thisrec.ALT[0])), thisrec.REF, str(thisrec.ALT[0]))
             outfile.write("\t".join([gene, sample, proteffect, mapping[rdf[sample][gene]["EFF"]], str(thisrec.CHROM), str(thisrec.POS), str(thisrec.POS+len(thisrec.ALT[0])), thisrec.REF, str(thisrec.ALT[0])] )+"\n")
     #print "##############################"
